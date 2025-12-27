390972e2e668214a5e90cd167dd7dfe6bcb8cef4
Amir <Amir93@yandex.ru>
2025-12-27 14:53:50 +0300

    admin + migrate

--- catalog/models.py
+++ catalog/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название", help_text="Название товара")
-    description = models.TextField(verbose_name="Описание")
-    photo = models.ImageField(upload_to="catalog/photo", blank=True, null=True, verbose_name="Фото")
+    name = models.CharField(max_length=100, verbose_name="Наименование", help_text="Название товара")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    photo = models.ImageField(upload_to="catalog/photo", blank=True, null=True, verbose_name="Изображение")
     category = models.ForeignKey(
         "Category", on_delete=models.SET_NULL, verbose_name="Категория", blank=True, null=True, related_name="products"
     )
-    purchase_price = models.IntegerField()
-    created_at = models.DateField(blank=True, verbose_name="Дата создания")
-    updated_at = models.DateField(blank=True, verbose_name="Дата создания")
+    price = models.IntegerField(blank=True, null=True, verbose_name='Цена за покупку')
+    created_at = models.DateField(blank=True, null=True, verbose_name="Дата создания")
+    updated_at = models.DateField(blank=True, null=True, verbose_name="Дата последнего изменения")
 
     def __str__(self):
         return self.name
@@ -21,8 +21,8 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название", help_text="Название товара")
-    description = models.TextField()
+    name = models.CharField(max_length=100, verbose_name="Наименование", help_text="Название товара")
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return self.name
